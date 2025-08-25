82ba331594767c1e113ab305564741985eb45ddd
Xael <67170424+Xael@users.noreply.github.com>
2025-08-25 20:58:02 -0300

    Add files via upload

--- server.js
+++ server.js
@@ -889,5 +889,5 @@ io.on('connection', (socket) => {
 const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => {
   console.log(`Servidor rodando na porta ${PORT}`);
-  testConnection();
+  db.testConnection();
 });
