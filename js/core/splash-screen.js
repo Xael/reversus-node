934f9a7d635b54a89736e2d78e8b5c8d4bdac9dd
Xael <67170424+Xael@users.noreply.github.com>
2025-09-06 17:26:07 -0300

    Delete js directory

--- js/core/splash-screen.js
+++
@@ -1,4 +0,0 @@
-// This file acts as a shim to re-export the function from its new location.
-// It ensures that older, cached versions of files that still import from 
-// 'js/core/splash-screen.js' will not break.
-export { showSplashScreen } from '../ui/splash-screen.js';
